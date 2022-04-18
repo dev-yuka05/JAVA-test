10cba077a9b1c188297b3ccd5aa160ad205d5b1a
Minjun <84955607+devkr-potato@users.noreply.github.com>
2022-04-18 15:06:43 +0900

    Add files via upload

---
+++ school/operator/OperatorTest05.java
@@ -0,0 +1,29 @@
+package operator;
+
+public class OperatorTest05 {
+
+	public static void main(String[] args) {
+		// 비교연산자 >, <, >=, <=, ==, !=
+		int num1 = 10;
+		int num2 = 10;
+		
+		boolean result1 = (num1 == num2);
+		boolean result2 = (num1 != num2);
+		boolean result3 = (num1 <= num2);
+		
+		System.out.println(result1);
+		System.out.println(result2);
+		System.out.println(result3);
+		
+		
+		char char1 = 'A';
+		char char2 = 'B';
+		
+		boolean result4 = char1 < char2;
+		
+		System.out.println(result4);
+		
+		
+	}
+
+}
