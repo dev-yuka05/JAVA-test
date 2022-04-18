60feaa5d38b27c11b009229c40174150cb2ff4c1
Minjun <84955607+devkr-potato@users.noreply.github.com>
2022-04-18 15:55:40 +0900

    Add files via upload

---
+++ school/operator/OperatorTest08.java
@@ -0,0 +1,18 @@
+package operator;
+
+public class OperatorTest08 {
+
+	public static void main(String[] args) {
+		// 22년 정처리1회 실기 11번 문제
+		
+		int num1 = 3;
+		int num2 = 7;
+		
+		if(++num1 < 5 || ++num2 > 8)
+			System.out.println(num1);
+		
+		System.out.println(num2);
+
+	}
+
+}
