384dcc7be4ef97b657f57ca8e1ea017e404cf503
Minjun <84955607+devkr-potato@users.noreply.github.com>
2022-04-19 15:18:52 +0900

    Create OperatorTest09.java

---
+++ school/operator/OperatorTest09.java
@@ -0,0 +1,27 @@
+package operator;
+
+public class OperatorTest01 {
+
+	public static void main(String[] args) {
+		// 대입연산자 단순대입 (=) 복합대입 (+= =+ etc...)
+		
+		int result = 0;
+		
+		result += 10;
+		System.out.println(result);
+		
+		result -= 5;
+		System.out.println(result);
+		
+		result *= 5;
+		System.out.println(result);
+		
+		result /= 5;
+		System.out.println(result);
+		
+		result %= 3;
+		System.out.println(result);
+		
+	}
+
+}
