3bab157aef90e9f5a9f29ba679497012d53301b7
Minjun <84955607+dev-yuka05@users.noreply.github.com>
2022-06-20 16:38:01 +0900

    Add files via upload

---
+++ school/array/ArrrayEx01.java
@@ -0,0 +1,14 @@
+package arrayex;
+
+public class ArrrayEx01 {
+
+	public static void main(String[] args) {
+//		int[] age = null;
+//		int[] age = {17,28,25,30,45};
+		int[] age = new int[] {17,22,34,56,80};
+		
+		System.out.println(age[1]);
+
+	}
+
+}
