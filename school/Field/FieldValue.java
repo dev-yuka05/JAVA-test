e72087b6ee2828838347543503de895d54616ce5
Minjun <84955607+dev-yuka05@users.noreply.github.com>
2022-06-28 15:36:59 +0900

    Add files via upload

---
+++ school/Field/FieldValue.java
@@ -0,0 +1,11 @@
+package FieldTest;
+
+public class FieldValue {
+	// ÇÊµå
+	byte bytefield;
+	char charfield;
+	short shortfield;
+	float floatfield = 3.14f;
+	boolean booleanfield;
+	int[] arrfield;
+}
---
+++ school/Field/Fieldexample.java
@@ -0,0 +1,18 @@
+package FieldTest;
+
+public class Fieldexample {
+
+	public static void main(String[] args) {
+		// 필드
+		FieldValue fiv = new FieldValue();
+		
+		System.out.println("byte 타입 필드선언 : "+fiv.bytefield);
+		System.out.println("char 타입 필드선언 : "+fiv.charfield);
+		System.out.println("short 타입 필드선언 : "+fiv.shortfield);
+		System.out.println("float 타입 필드선언 : "+fiv.floatfield);
+		System.out.println("boolean 타입 필드선언 : "+fiv.booleanfield);
+		System.out.println("배열 타입 필드선언 : "+fiv.arrfield);
+
+	}
+
+}
